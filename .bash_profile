97331007363f1c00f482632922b833a0b53e8c6a
fooabobabar <fooabobabar@gmail.com>
2026-05-22 20:57:09 -0300

    Update .emacs and add noto-fonts-emoji package

--- .bash_profile
+++ .bash_profile
@@ -3,12 +3,3 @@
 #
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
-
-# Added by LM Studio CLI (lms)
-export PATH="$PATH:/home/streamer/.lmstudio/bin"
-# End of LM Studio CLI section
-
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
--- .emacs.custom.el
+++ .emacs.custom.el
@@ -30,4 +30,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-code-face ((t (:family "Iosevka"))))
+ '(markdown-inline-code-face ((t (:family "Iosevka")))))
